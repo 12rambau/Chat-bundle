4a662f79c1235ad3dadce06b75cf70d8512fdac6
12rambau <pierrick.rambaud@orange.fr>
2019-12-12 16:59:20 +0100

    add tests

---
+++ tests/ChatTest.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Btba\ChatBundle\Tests;
+
+use PHPUnit\Framework\TestCase;
+
+class ChatTest extends TestCase
+{
+    public function test()
+    {
+        $this->assertEquals(0,0);
+    }
+}
