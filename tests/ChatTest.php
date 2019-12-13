02cfa5d0601e409b491fb75f3f5b91077c6e7957
12rambau <pierrick.rambaud@orange.fr>
2019-12-13 11:19:11 +0100

    add teardown function that clear the cache

--- tests/ChatTest.php
+++ tests/ChatTest.php
@@ -7,18 +7,41 @@ use Btba\ChatBundle\Controller\ChatController;
 use Btba\ChatBundle\Form\ChatMessageType;
 use PHPUnit\Framework\TestCase;
 use Symfony\Component\Config\Loader\LoaderInterface;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\HttpKernel\Kernel;
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 
 class ChatTest extends TestCase
 {
+    public function tearDown()
+    {
+        //empty the cache 
+        $dir = __DIR__ . '/cache/';
+
+        foreach (new \RecursiveIteratorIterator(new \RecursiveDirectoryIterator($dir, \FilesystemIterator::SKIP_DOTS), \RecursiveIteratorIterator::CHILD_FIRST) as $path)
+            $path->isDir() && !$path->isLink() ? rmdir($path->getPathname()) : unlink($path->getPathname());
+    }
     public function test()
     {
         $this->assertEquals(0, 0);
     }
 
-    public function testServiceWiring()
-    { 
-        $kernel = new BtbaChatTestingKernel('test', true);
+    /*public function testServiceWiring()
+    {
+        $kernel = new BtbaChatTestingKernel();
+        $kernel->boot();
+        $container = $kernel->getContainer();
+
+    }*/
+    public function testServiceWiringWithConfiguration()
+    {
+
+
+        $kernel = new BtbaChatTestingKernel([
+            'update_interval' => 1000,
+            'message_class' => 'App\Entity\Message.php',
+            'author_class' => 'App\Entity\Author.php'
+        ]);
         $kernel->boot();
         $container = $kernel->getContainer();
 
@@ -33,12 +56,24 @@ class ChatTest extends TestCase
 
 class BtbaChatTestingKernel extends Kernel
 {
+    private $btbaChatConfig;
+
+    public function __construct(array $btbaChatConfig = [])
+    {
+        $this->btbaChatConfig = $btbaChatConfig;
+
+        parent::__construct('test', true);
+    }
     public function registerBundles()
-    { 
+    {
         return [
             new BtbaChatBundle()
         ];
     }
     public function registerContainerConfiguration(LoaderInterface $loader)
-    { }
+    {
+        $loader->load(function (ContainerBuilder $container) {
+            $container->loadFromExtension('btba_chat', $this->btbaChatConfig);
+        });
+    }
 }
